b7117f529abeba6a1a179820ac5c8e4bdc52c58d
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-09-25 21:19:18 -0300

    create homepage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="vue-app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
 import './styles/global.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 document.title = 'Book Explorer';
 
@@ -14,6 +16,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Home,
   },
 };
 </script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <div>
+      <h1>Guia de Livros</h1>
+      <h2>
+        Encontre o livro que você procura. Busque por nome, autor ou
+        publicadora.
+      </h2>
+      <form @submit.prevent>
+        <input type="text" name="search" />
+        <button type="submit">
+          Buscar Livro
+        </button>
+      </form>
+    </div>
+    <div>
+      <img src="@/assets/books.png" alt="Livros" id="books" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default { name: 'Home' };
+</script>
+
+<style>
+main {
+  width: min(100%, 1100px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 1rem;
+  margin: auto;
+}
+
+main > * {
+  flex: 1;
+}
+
+main div:last-child img {
+  width: 100%;
+}
+
+h1,
+h2 {
+  color: var(--text-dark);
+}
+
+h1 {
+  font-size: 3.5rem;
+  font-weight: 600;
+}
+h2 {
+  font-size: 1.25rem;
+  font-weight: 400;
+}
+</style>
